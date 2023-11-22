92db9f6a642bdb4ff35f384d1712245d6eb7240e
nev-awaken <nevin2001t@gmail.com>
2023-11-22 14:49:47 -0500

    added five rounds of game and a score board

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 console.log("Rock, paper, scissor....")
 let moves = ["rock","paper","scissor"];
 let user_input = prompt("Choose your attack move:");
+let score_board = {
+    computer: 0,
+    user: 0
+};
 
 
 
@@ -17,27 +21,46 @@ console.log("User Move",user_input);
 
 
 
-if(user_input != moves[0] && user_input != moves[1] && user_input != moves[2]){
-    console.log("Wrong move bro");
-}else{
-    if(user_input == computer_move ){
-        console.log("Draw");  
-    }else if(user_input == "rock" && computer_move  == "scissor"){
-       console.log("User wins");
-    }else if(user_input == "rock" && computer_move  == "paper"){
-        console.log("User loose");
-    }else if(user_input == "paper" && computer_move == "scissor"){
-        console.log("User loose");
-    }else if(user_input == "paper" && computer_move == "rock"){
-        console.log("User wins");
-    }else if(user_input == "scissor" && computer_move == "rock"){
-        console.log("User loose");
-    }else if(user_input == "scissor" && computer_move == "paper"){
-            console.log("User wins");
+for(i = 0; i<= 5; i++){
+    let user_input = prompt("Choose your attack move:");
+    if(user_input != moves[0] && user_input != moves[1] && user_input != moves[2]){
+        console.log("Wrong move bro");
+        i = --i;
+        
     }else{
-        console.log("User loose")
+        if(user_input == computer_move ){
+            console.log("Draw");
+            i = i--;
+            
+        }else if(user_input == "rock" && computer_move  == "scissor"){
+           console.log("User wins");
+           score_board.user = score_board.user  + 1;
+        }else if(user_input == "rock" && computer_move  == "paper"){
+            console.log("User loose");
+            score_board.computer = score_board.computer  + 1;
+        }else if(user_input == "paper" && computer_move == "scissor"){
+            console.log("User loose");
+            score_board.computer = score_board.computer  + 1;
+        }else if(user_input == "paper" && computer_move == "rock"){
+            console.log("User wins");
+            score_board.user = score_board.user  + 1;
+        }else if(user_input == "scissor" && computer_move == "rock"){
+            console.log("User loose");
+            score_board.computer = score_board.computer  + 1;
+        }else if(user_input == "scissor" && computer_move == "paper"){
+                console.log("User wins");
+                score_board.user = score_board.user  + 1;
+        }else{
+            console.log("User loose")
+            score_board.computer = score_board.computer  + 1;
+        }
     }
 }
+
+console.log("*Final Score Board*");
+console.log("User Score :" + score_board.user, "Comp Score :" + score_board.computer);
+
+
 
 
 
